37cae7c9b7a0bfa86f6318f3aa907c7d8be18822
zuri06 <bothcv168@gmail.com>
2026-07-16 16:14:29 +0700

    feat: create DemoWeddingSeeder to populate initial wedding, guest, and RSVP data

--- database/seeders/DemoWeddingSeeder.php
+++ database/seeders/DemoWeddingSeeder.php
@@ -11,6 +11,7 @@ use App\Enums\RsvpStatus;
 use App\Enums\SubscriptionStatus;
 use App\Enums\TimelineCategory;
 use App\Enums\WeddingStatus;
+use App\Enums\ExpenseStatus;
 use App\Models\InvitationTemplate;
 use App\Models\Package;
 use App\Models\Role;
@@ -105,23 +106,38 @@ class DemoWeddingSeeder extends Seeder
         });
 
         $guestRows = [
-            ['Chenda Sok', 'Family', '+855 11 111 111', true],
-            ['Bopha Lim', 'Family', '+855 11 222 222', false],
-            ['Rithy Heng', 'Friends', '+855 11 333 333', false],
-            ['Sreyneang Pich', 'Friends', '+855 11 444 444', false],
-            ['Samnang Oun', 'VIP', '+855 11 555 555', true],
-            ['Kunthea Mao', 'Company', '+855 11 666 666', false],
-            ['Vibol Tan', 'Friends', '+855 11 777 777', false],
-            ['Malis Chea', 'Family', '+855 11 888 888', false],
+            ['Lok Oknha Chan Dara', 'Family', '+855 12 999 001', 'dara.chan@example.com', 'Villa 45A, St 310, Boeung Keng Kang, Phnom Penh', 'Father of the Bride', true],
+            ['Lok Chumteav Lim Sreymom', 'Family', '+855 12 999 002', 'sreymom.lim@example.com', 'Villa 45A, St 310, Boeung Keng Kang, Phnom Penh', 'Mother of the Bride', true],
+            ['Mr. Kim Sovann', 'Family', '+855 11 888 001', 'sovann.kim@example.com', 'House 12, St 598, Toul Kork, Phnom Penh', 'Father of the Groom', true],
+            ['Mrs. Sok Chantha', 'Family', '+855 11 888 002', 'chantha.sok@example.com', 'House 12, St 598, Toul Kork, Phnom Penh', 'Mother of the Groom', true],
+            ['Chan Rotha', 'Family', '+855 77 111 222', 'rotha.chan@example.com', 'Phnom Penh', 'Brother of the Bride', false],
+            ['Kim Sreypich', 'Family', '+855 88 333 444', 'sreypich.kim@example.com', 'Phnom Penh', 'Sister of the Groom', false],
+            ['H.E. Keo Puthy', 'VIP', '+855 12 222 333', 'puthy.keo@gov.kh', 'Phnom Penh', 'VIP Guest from Ministry', true],
+            ['Dr. Seng Sarin', 'VIP', '+855 12 555 666', 'sarin.seng@example.com', 'Phnom Penh', 'Family Doctor', true],
+            ['Madam Oung Sopheap', 'VIP', '+855 17 777 888', 'sopheap.oung@example.com', 'Phnom Penh', 'Close family friend', true],
+            ['Rithy Heng', 'Friends', '+855 93 111 222', 'rithy.heng@example.com', 'Phnom Penh', 'Best Man / Groomsman', false],
+            ['Sreyneang Pich', 'Friends', '+855 95 333 444', 'sreyneang.pich@example.com', 'Phnom Penh', 'Maid of Honor / Bridesmaid', false],
+            ['Chenda Sok', 'Friends', '+855 98 555 666', 'chenda.sok@example.com', 'Phnom Penh', 'MC for the Reception dinner', false],
+            ['Malis Chea', 'Friends', '+855 70 777 888', 'malis.chea@example.com', 'Siem Reap', 'High school friend traveling from Siem Reap', false],
+            ['Sokha Pen', 'Friends', '+855 15 999 111', 'sokha.pen@example.com', 'Phnom Penh', 'University classmate', false],
+            ['Chan Chhaya', 'Friends', '+855 16 222 333', 'chhaya.chan@example.com', 'Phnom Penh', 'Photographer friend', false],
+            ['Phirun Seng', 'Friends', '+855 92 444 555', 'phirun.seng@example.com', 'Phnom Penh', 'Groom\'s soccer club friend', false],
+            ['Kunthea Mao', 'Company', '+855 12 444 888', 'kunthea.mao@company.com', 'Phnom Penh', 'Bride\'s Manager at Work', false],
+            ['Vibol Tan', 'Company', '+855 87 555 999', 'vibol.tan@company.com', 'Phnom Penh', 'Bride\'s colleague (Developer)', false],
+            ['Samnang Oun', 'Company', '+855 96 333 222', 'samnang.oun@company.com', 'Phnom Penh', 'Bride\'s colleague (HR Director)', false],
+            ['Tep Sovann', 'Company', '+855 99 777 666', 'sovann.tep@company.com', 'Phnom Penh', 'Groom\'s colleague (Designer)', false],
+            ['Nguon Rith', 'Company', '+855 78 888 999', 'rith.nguon@company.com', 'Phnom Penh', 'Groom\'s colleague (QA)', false],
         ];
 
         $guests = [];
-        foreach ($guestRows as [$name, $groupName, $phone, $isVip]) {
+        foreach ($guestRows as [$name, $groupName, $phone, $email, $address, $note, $isVip]) {
             $guests[$name] = $wedding->guests()->updateOrCreate(
                 ['name' => $name],
                 [
                     'phone' => $phone,
-                    'email' => strtolower(str_replace(' ', '.', $name)).'@example.com',
+                    'email' => $email,
+                    'address' => $address,
+                    'note' => $note,
                     'guest_group_id' => $groups[$groupName]->id,
                     'invitation_id' => $invitation->id,
                     'is_vip' => $isVip,
@@ -130,10 +146,18 @@ class DemoWeddingSeeder extends Seeder
         }
 
         $rsvpRows = [
-            ['Chenda Sok', RsvpStatus::Accepted, 2, 'So happy for you both!'],
-            ['Rithy Heng', RsvpStatus::Accepted, 1, 'Wouldn\'t miss it.'],
-            ['Sreyneang Pich', RsvpStatus::Maybe, 1, 'Will confirm next week.'],
-            ['Kunthea Mao', RsvpStatus::Declined, 1, 'Traveling that week, sorry!'],
+            ['Lok Oknha Chan Dara', RsvpStatus::Accepted, 2, 'Looking forward to our family\'s big day!'],
+            ['Mr. Kim Sovann', RsvpStatus::Accepted, 2, 'Can\'t wait to celebrate!'],
+            ['H.E. Keo Puthy', RsvpStatus::Accepted, 4, 'Honored to attend your wedding, blessing the couple.'],
+            ['Rithy Heng', RsvpStatus::Accepted, 1, 'Best man is ready! See you there!'],
+            ['Sreyneang Pich', RsvpStatus::Accepted, 1, 'So excited for you, Sophea! Sending love.'],
+            ['Chenda Sok', RsvpStatus::Accepted, 1, 'Honored to host the stage as MC.'],
+            ['Kunthea Mao', RsvpStatus::Accepted, 2, 'Congratulations to the lovely couple!'],
+            ['Vibol Tan', RsvpStatus::Declined, 1, 'Sorry, I will be abroad on a business trip.'],
+            ['Samnang Oun', RsvpStatus::Maybe, 2, 'Will check schedule and confirm by next week.'],
+            ['Dr. Seng Sarin', RsvpStatus::Accepted, 2, 'Will attend the evening reception.'],
+            ['Malis Chea', RsvpStatus::Accepted, 1, 'Wouldn\'t miss it for the world, coming from Siem Reap!'],
+            ['Chan Chhaya', RsvpStatus::Accepted, 2, 'Happy for you both!'],
         ];
 
         foreach ($rsvpRows as $index => [$guestName, $status, $count, $message]) {
@@ -146,13 +170,19 @@ class DemoWeddingSeeder extends Seeder
                     'number_of_guests' => $count,
                     'message' => $message,
                     'status' => $status->value,
-                    'responded_at' => now()->subDays(10 - $index * 2),
+                    'responded_at' => now()->subDays(12 - $index),
                 ],
             );
         }
 
         $tables = [];
-        foreach ([['Family Table', 1, 10], ['Friends Table', 2, 10], ['VIP Table', 3, 8]] as [$name, $number, $capacity]) {
+        foreach ([
+            ['VIP Table', 1, 10],
+            ['Bride Family Table', 2, 10],
+            ['Groom Family Table', 3, 10],
+            ['Friends Table', 4, 10],
+            ['Company Table', 5, 10]
+        ] as [$name, $number, $capacity]) {
             $tables[$name] = $wedding->tables()->updateOrCreate(
                 ['table_name' => $name],
                 ['table_number' => $number, 'capacity' => $capacity],
@@ -160,11 +190,20 @@ class DemoWeddingSeeder extends Seeder
         }
 
         $seatPlan = [
-            ['Chenda Sok', 'Family Table', 1],
-            ['Bopha Lim', 'Family Table', 2],
-            ['Malis Chea', 'Family Table', 3],
+            ['Lok Oknha Chan Dara', 'VIP Table', 1],
+            ['Lok Chumteav Lim Sreymom', 'VIP Table', 2],
+            ['Mr. Kim Sovann', 'VIP Table', 3],
+            ['Mrs. Sok Chantha', 'VIP Table', 4],
+            ['Dr. Seng Sarin', 'VIP Table', 5],
+            ['H.E. Keo Puthy', 'VIP Table', 6],
+            ['Chan Rotha', 'Bride Family Table', 1],
+            ['Kim Sreypich', 'Groom Family Table', 1],
             ['Rithy Heng', 'Friends Table', 1],
-            ['Samnang Oun', 'VIP Table', 1],
+            ['Sreyneang Pich', 'Friends Table', 2],
+            ['Chenda Sok', 'Friends Table', 3],
+            ['Malis Chea', 'Friends Table', 4],
+            ['Kunthea Mao', 'Company Table', 1],
+            ['Samnang Oun', 'Company Table', 2],
         ];
 
         foreach ($seatPlan as [$guestName, $tableName, $seat]) {
@@ -175,23 +214,82 @@ class DemoWeddingSeeder extends Seeder
         }
 
         $giftRows = [
-            ['Chenda Sok', GiftType::Cash, 200, null],
-            ['Samnang Oun', GiftType::BankTransfer, 500, null],
-            ['Rithy Heng', GiftType::Item, null, 'Rice cooker'],
+            ['Lok Oknha Chan Dara', GiftType::Cash, 2000.00, 'USD', null, 'Wedding blessing from mom and dad'],
+            ['Mr. Kim Sovann', GiftType::Cash, 1500.00, 'USD', null, 'For your new beginning together'],
+            ['H.E. Keo Puthy', GiftType::Cash, 500.00, 'USD', null, 'Congratulations! Wishing you a long life together.'],
+            ['Chenda Sok', GiftType::BankTransfer, 200.00, 'USD', null, 'Wishing you happiness!'],
+            ['Rithy Heng', GiftType::BankTransfer, 100.00, 'USD', null, 'Congrats brother! Happy marriage!'],
+            ['Kunthea Mao', GiftType::BankTransfer, 150.00, 'USD', null, 'Happy married life!'],
+            ['Dr. Seng Sarin', GiftType::Cash, 800000.00, 'KHR', null, 'Wishing you love and happiness always.'],
+            ['Madam Oung Sopheap', GiftType::Cash, 1200000.00, 'KHR', null, 'Big congrats to Visal and Sophea!'],
+            ['Malis Chea', GiftType::BankTransfer, 400000.00, 'KHR', null, 'So happy for you guys!'],
+            ['Chan Chhaya', GiftType::BankTransfer, 50.00, 'USD', null, 'Blessing to the couple!'],
+            ['Sreyneang Pich', GiftType::Item, null, 'USD', 'Premium Dining Set', 'A little something for your new kitchen.'],
+            ['Tep Sovann', GiftType::Item, null, 'USD', 'Nespresso Coffee Machine', 'Fuel for your early mornings!'],
         ];
 
-        foreach ($giftRows as [$guestName, $type, $amount, $item]) {
+        foreach ($giftRows as [$guestName, $type, $amount, $currency, $item, $note]) {
             $wedding->gifts()->updateOrCreate(
                 ['guest_id' => $guests[$guestName]->id, 'gift_type' => $type->value],
-                ['amount' => $amount, 'item_name' => $item, 'received_at' => now()->subDays(3)],
+                [
+                    'amount' => $amount,
+                    'currency' => $currency,
+                    'item_name' => $item,
+                    'note' => $note,
+                    'received_at' => now()->subDays(3)
+                ],
             );
         }
 
         $timelineEvents = [
-            [TimelineCategory::Engagement, 'Engagement Ceremony', 'Traditional Khmer engagement at the bride\'s family home.', now()->addDays(44)->setTime(8, 0), 'Bride\'s family home', 1],
-            [TimelineCategory::Ceremony, 'Wedding Ceremony', 'Monks\' blessing and traditional ceremonies.', now()->addDays(45)->setTime(7, 30), 'Wat Botum Park Hall', 2],
-            [TimelineCategory::Reception, 'Reception Dinner', 'Dinner, toasts and dancing.', now()->addDays(45)->setTime(17, 30), 'Sokha Hotel Grand Ballroom', 3],
-            [TimelineCategory::AfterParty, 'After Party', 'Live band and late-night snacks.', now()->addDays(45)->setTime(21, 30), 'Sokha Hotel Sky Bar', 4],
+            [
+                TimelineCategory::Engagement,
+                'Groom\'s Procession (Hai Khan Mla)',
+                'Traditional parade bringing gifts from the groom\'s family to the bride\'s house.',
+                now()->addDays(44)->setTime(8, 0),
+                'Bride\'s Family Villa, Toul Kork',
+                1
+            ],
+            [
+                TimelineCategory::Engagement,
+                'Ring Exchange & Tea Ceremony (Sien Don Ta)',
+                'Paying respects to ancestors and exchanging wedding rings in front of families.',
+                now()->addDays(44)->setTime(10, 0),
+                'Bride\'s Family Villa, Toul Kork',
+                2
+            ],
+            [
+                TimelineCategory::Ceremony,
+                'Hair Cutting Ceremony (Gat Sah)',
+                'Symbolic cleansing ceremony performed by parents and guests to bring good luck.',
+                now()->addDays(45)->setTime(8, 0),
+                'Wat Botum Park Hall, Phnom Penh',
+                3
+            ],
+            [
+                TimelineCategory::Ceremony,
+                'Monks\' Blessing (Chang Han)',
+                'Buddhist monks chant blessings and sprinkle holy water on the couple for prosperity.',
+                now()->addDays(45)->setTime(9, 30),
+                'Wat Botum Park Hall, Phnom Penh',
+                4
+            ],
+            [
+                TimelineCategory::Reception,
+                'Reception Grand Dinner',
+                'Banquet celebration, speeches, cake cutting, guest dining, and live band performance.',
+                now()->addDays(45)->setTime(17, 30),
+                'Sokha Hotel Grand Ballroom',
+                5
+            ],
+            [
+                TimelineCategory::AfterParty,
+                'After Party & Dancing',
+                'DJ set, dancing, cocktails and late-night snacks with close friends.',
+                now()->addDays(45)->setTime(21, 30),
+                'Sokha Hotel Sky Bar',
+                6
+            ],
         ];
 
         foreach ($timelineEvents as [$category, $title, $description, $startsAt, $location, $order]) {
@@ -226,6 +324,34 @@ class DemoWeddingSeeder extends Seeder
                 'paid_at' => null,
             ],
         );
+
+        $expenses = [
+            ['Wedding Venue Booking', 'Sokha Hotel', 12000.00, 5000.00, ExpenseStatus::Partial, 'Deposit paid. Balance due 1 week before event.', now()->subDays(30), 'USD'],
+            ['Catering & Drinks (60 Tables)', 'Sokha Catering', 15000.00, 0.00, ExpenseStatus::Planned, 'Estimated cost. Awaiting final guest count.', now()->addDays(10), 'USD'],
+            ['Wedding Planner Services', 'Dara Organizer', 2500.00, 2500.00, ExpenseStatus::Paid, 'Paid in full.', now()->subDays(25), 'USD'],
+            ['Floral & Stage Decoration', 'Romdoul Florist', 4500.00, 2000.00, ExpenseStatus::Partial, 'Deposit paid for customized stage background.', now()->subDays(15), 'USD'],
+            ['Photography & Video Services', 'Khmer Wedding Studio', 3000.00, 1500.00, ExpenseStatus::Partial, 'Includes pre-wedding shoot + 2 days event coverage.', now()->subDays(20), 'USD'],
+            ['Bride & Groom Custom Attire', 'Queen Boutique', 3500.00, 3500.00, ExpenseStatus::Paid, 'Tailoring completed and clothes picked up.', now()->subDays(10), 'USD'],
+            ['Bridal Makeup & Styling', 'Paris Makeup', 1200.00, 600.00, ExpenseStatus::Partial, 'Paid deposit for 2-day session.', now()->subDays(12), 'USD'],
+            ['Invitation Cards Printing', 'Phsar Thmey Print', 500.00, 500.00, ExpenseStatus::Paid, '600 cards printed and delivered.', now()->subDays(18), 'USD'],
+            ['Live Band & Sound System', 'Classic Sound', 1800.00, 0.00, ExpenseStatus::Planned, 'Planned live orchestra + sound equipment hire.', now()->addDays(20), 'USD'],
+            ['Custom Wedding Rings', 'Koh Pich Jewelry', 8000000.00, 8000000.00, ExpenseStatus::Paid, 'Gold rings with diamond setting.', now()->subDays(8), 'KHR'],
+        ];
+
+        foreach ($expenses as [$itemName, $vendor, $amount, $paidAmount, $status, $note, $spentAt, $currency]) {
+            $wedding->expenses()->updateOrCreate(
+                ['item_name' => $itemName],
+                [
+                    'vendor' => $vendor,
+                    'amount' => $amount,
+                    'paid_amount' => $paidAmount,
+                    'status' => $status->value,
+                    'note' => $note,
+                    'spent_at' => $spentAt,
+                    'currency' => $currency,
+                ]
+            );
+        }
     }
 
     private function user(string $name, string $email, RoleKey $roleKey): User
